3a0b92d13342397949fb79b9dd880524b16adc5e
erdeni <erdeni@bk.ru>
2025-12-15 23:00:18 +0800

    Добавлена документация и скрипты компиляции и запуска

---
+++ make.util.bat
@@ -0,0 +1,1 @@
+javac -sourcepath src -d bin src\Main.java && "c:\Program Files\Java\jdk-21\bin\jar.exe" cfe util.jar Main -C bin .
---
+++ run.util.bat
@@ -0,0 +1,1 @@
+java -jar .\util.jar -f -p "result_" -o "result" in1.txt in2.txt in3.txt
